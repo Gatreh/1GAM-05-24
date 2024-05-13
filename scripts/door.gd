67650dbd56f3fba01a214e1b89c209456b622ab7
Kim "Gateh" Häggblom <gatreh@proton.me>
2024-05-14 00:05:50 +0200

    Removed unecessary and unused parts of door signals

--- scripts/door.gd
+++ scripts/door.gd
@@ -5,10 +5,7 @@ extends Area2D
 
 func _on_body_entered(body):
 	animated_sprite_2d.play("open")
-	print("body entered")
-	emit_signal("next_level")
 
 
 func _on_body_exited(body):
 	animated_sprite_2d.play("close")
-	print("body exited")
