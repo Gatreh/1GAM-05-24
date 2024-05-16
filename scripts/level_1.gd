bb5a58d5196b295f573c431445924d6bc624b758
Mia <m3and3rin9mia@gmail.com>
2024-05-16 13:21:26 +0200

    Level 2 Expansion

---
+++ scripts/level_1.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var tile_map = $TileMap
+@onready var total_coins := get_tree().get_nodes_in_group("coins")
+
+func _ready():
+	GameManager.start_music(1)
+
+func _on_next_level_door_body_entered(body):
+	if %NextLevelDoor.allow_open:
+		await get_tree().create_timer(1.0).timeout
+		GameManager.next_level(2)
